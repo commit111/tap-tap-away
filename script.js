83ca258c21d89b42857290a13ea5d42c8a2b53c6
Glitch (glitch-hello-website) <none>
2023-02-05 21:58:31 +0000

    📌✌️ Checkpoint ./script.js:24111152/1124 ./index.html:24111152/61

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 window.addEventListener("load", () => {
   const sounds = document.querySelectorAll(".sound");
   const pads = document.querySelectorAll(".pads div");
-  
-  console.log(sounds[0]);
+  const visual = document.querySelectorAll(".visual");
+  const colors = [
+    "rgba(253, 239, 253)",
+    "rgba(200, 243, 246)",
+    "rgba(255, 255, 197)",
+    "rgba(255, 220, 220)",
+    "rgba(219, 209, 255)",
+    "rgba(255, 197, 239)",
+  ];
   
   //make sounds work
   pads.forEach(pad =>{
     pad.addEventListener("click", function(){ //use normal function to use "this" to refer to parent pad
-      sounds[4];
+      this.querySelector("audio").currentTime = 0;
+      this.querySelector("audio").play();
     });
   });
+  
+  //create a function to make bubbles
+  const createBubbles = () => {
+    const bubble = document.create
+  }
+  
   
 });
